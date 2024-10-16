0ccb173a4ec4868fdf3982cddcaa2a3ae465fcf9
Graham Arnett <GrahamKentArnett@outlook.com>
2024-10-15 22:42:25 -0400

    init commit

---
+++ GradeConverter/GradeConverter/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace GradeConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the grade converter app");
+            String continueChoice;
+           
+            do {
+                Console.Write("Please enter your grade as an integer: ");
+                int score = int.Parse(Console.ReadLine());
+                String grade;
+
+                if(score < 60)
+                {
+                    grade = "F";
+                }
+                else if (score <= 67)
+                {
+                    grade = "D";
+                }
+                else if (score <= 79)
+                {
+                    grade = "C";
+                }
+                else if (score <= 87)
+                {
+                    grade = "B";
+                }
+               else
+                {
+                    grade = "A";
+                }
+
+                Console.WriteLine($"Your letter grade is: {grade}");
+                Console.Write("Would you like to go again? (y/n): ");
+                continueChoice = Console.ReadLine();  
+            } while (continueChoice == "y");
+
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
